c5d25c509445383f65ab8a0f07e020a1c9f940bb
Fabio Antonucci <fabio.antonucci@laserromae.it>
2025-12-04 18:22:59 +0100

    WIP: implement first task.

--- day_3/main.go
+++ day_3/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+
+	"github.com/fantonucci89/advent_of_code_2025/internal/utils"
+)
+
+func findHigherNumber(line string) (int, error) {
+	tens, _ := strconv.Atoi(line[0:1])
+	unit, _ := strconv.Atoi(line[1:2])
+
+	for i := 2; i < len(line)-1; i++ {
+		charInt, _ := strconv.Atoi(string(line[i]))
+		if charInt > tens {
+			tens = charInt
+			nextCharInt, _ := strconv.Atoi(string(line[i+1]))
+			unit = nextCharInt
+			fmt.Println(tens, unit)
+			continue
+		}
+		if charInt > unit {
+			unit = charInt
+			fmt.Println(tens, unit)
+			continue
+		}
+	}
+
+	result, err := fmt.Printf("%d%d", tens, unit)
+	if err != nil {
+		return 0, err
+	}
+	fmt.Println("End iteration")
+	return result, nil
+}
 
 func main() {
-	fmt.Println("Hello, Day 3!")
+	file, err := utils.ReadData("data.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	counter := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		res, err := findHigherNumber(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		counter += res
+	}
+	fmt.Println(counter)
 }
