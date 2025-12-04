22dc1a1cfa1676aab31cbc6e7614876a79b2735e
Fabio Antonucci <fabio.antonucci@laserromae.it>
2025-12-04 12:12:10 +0100

    refactor code adding executable files

--- day_2/main.go
+++ day_2/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
-var task int = 1
+var (
+	TaskStr string
+	Task    int = 1
+)
 
 func ReadData() (*os.File, error) {
 	file, err := os.Open("data.txt")
@@ -41,7 +43,7 @@ func ListRanges(listOfRanges *[]string) {
 	}
 }
 
-func CalculateInvalidIdInRange(r *int, idRange string) {
+func CalculateInvalidIDInRange(r *int, idRange string) {
 	// Split the range into start and end
 	parts := strings.Split(idRange, "-")
 	if len(parts) != 2 {
@@ -57,41 +59,41 @@ func CalculateInvalidIdInRange(r *int, idRange string) {
 
 	// Check for invalid IDs in the range
 	for id := start; id <= end; id++ {
-		if task == 1 && hasRepeatedDigits(id) {
+		if Task == 1 && hasRepeatedDigits(id) {
 			*r += id
 		}
-		if task == 2 && hasRepeatedDigitsImproved(id) {
+		if Task == 2 && hasRepeatedDigitsImproved(id) {
 			*r += id
 		}
 	}
 }
 
 func hasRepeatedDigits(id int) bool {
-	strId := strconv.Itoa(id)
-	if len(strId)%2 != 0 {
+	strID := strconv.Itoa(id)
+	if len(strID)%2 != 0 {
 		return false
 	}
-	mid := len(strId) / 2
-	return strId[:mid] == strId[mid:]
+	mid := len(strID) / 2
+	return strID[:mid] == strID[mid:]
 }
 
 func hasRepeatedDigitsImproved(id int) bool {
-	strId := strconv.Itoa(id)
+	strID := strconv.Itoa(id)
 
 	// Try all possible pattern lengths from 1 to half the string length
-	for patternLen := 1; patternLen <= len(strId)/2; patternLen++ {
+	for patternLen := 1; patternLen <= len(strID)/2; patternLen++ {
 		// Check if the string length is divisible by the pattern length
-		if len(strId)%patternLen != 0 {
+		if len(strID)%patternLen != 0 {
 			continue
 		}
 
 		// Extract the pattern
-		pattern := strId[:patternLen]
+		pattern := strID[:patternLen]
 
 		// Check if the entire string is made up of this pattern repeated
 		matched := true
-		for i := patternLen; i < len(strId); i += patternLen {
-			if strId[i:i+patternLen] != pattern {
+		for i := patternLen; i < len(strID); i += patternLen {
+			if strID[i:i+patternLen] != pattern {
 				matched = false
 				break
 			}
@@ -106,16 +108,17 @@ func hasRepeatedDigitsImproved(id int) bool {
 }
 
 func main() {
-	useTask := flag.Int("task", 1, "Task number (1 or 2)")
-	flag.Parse()
-
-	task = *useTask
+	if TaskStr != "" {
+		if t, err := strconv.Atoi(TaskStr); err == nil {
+			Task = t
+		}
+	}
 	var listOfRanges []string
 
 	ListRanges(&listOfRanges)
 	var result int
 	for _, r := range listOfRanges {
-		CalculateInvalidIdInRange(&result, r)
+		CalculateInvalidIDInRange(&result, r)
 	}
 	fmt.Println("Sum of invalid IDs:", result)
 }
